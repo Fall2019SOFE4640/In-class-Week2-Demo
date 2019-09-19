d1b446a1b931c72fa155acb4172d8eff5540ad14
AnwarBari <33730047+AnwarBari@users.noreply.github.com>
2019-09-18 20:07:56 -0400

    Commit message

--- app/src/main/java/com/example/sofe4640/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sofe4640/myapplication/MainActivity.java
@@ -1,3 +1,8 @@
+/*
+Author: Anwar Abdalbari
+Date: Dec. 17, 2019
+Purpose: Interface layouts
+ */
 package com.example.sofe4640.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
@@ -11,11 +16,13 @@ import java.lang.reflect.Array;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //The entry point to my app.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    //This method is used to handel linear layout activity
     public void callLinearLayout(View v){
         setContentView(R.layout.linear_layout);
         Spinner spinner = findViewById(R.id.spinnerId);
@@ -24,22 +31,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //This method is used to handel Gridview layout activity
     public void callGridViewLayout(View v){
         setContentView(R.layout.grid_layout);
 
     }
+
+    //This method is used to fil a sinner with array's data
     private void fillSpinner(Spinner spinner) {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.countries,R.layout.support_simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
          spinner.setAdapter(adapter);
-         //this is Java
-        // Car myCar = new Car();
     }
 
+    //This method is used to navigate back to the main activity
     public void backToMain(View v){
         setContentView(R.layout.activity_main);
     }
 
+    //This method is used to handel the relative layout activity
     public void callRelativeLayout(View v){
         setContentView(R.layout.relative_layout);
 
